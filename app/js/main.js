d0c9a1f3650b28ff4281b80c9111cfc43e781dc8
AlexeyLetov <adec78@mail.ru>
2020-09-20 22:49:21 +0700

    added homework 11

--- app/js/main.js
+++ app/js/main.js
@@ -5,6 +5,20 @@ $(function () {
     nextArrow: '<button class="slick-arrow slick-next"><img src="images/icons/chevron-right.svg" alt="next arrow"></button>'
   });
 
+  $(".followers__slider").slick({
+    prevArrow: '<button class="slick-arrow slick-prev"><img src="images/icons/chevron-left.svg" alt="prev arrow"></button>',
+    nextArrow: '<button class="slick-arrow slick-next"><img src="images/icons/chevron-right.svg" alt="next arrow"></button>',
+    slidesToShow: 3,
+    slidesToScroll: 3
+  });
+
+  $(".feedback__inner").slick({
+    prevArrow: '<button class="slick-arrow feedback__prev"><img src="images/icons/feedback-left.svg" alt="prev arrow"></button>',
+    nextArrow: '<button class="slick-arrow feedback__next"><img src="images/icons/feedback-right.svg" alt="next arrow"></button>',
+    slidesToShow: 2,
+    slidesToScroll: 2
+  });
+
   $(".rate-star").rateYo({
     rating: 4.5,
     starWidth: "14px",
@@ -21,9 +35,9 @@ $(function () {
     readOnly: true
   });
 
-  $('.title-box__filter').on("click", function () {
-    $('.filter-list').toggle();
-  });
+  // $('.title-box__filter').on("click", function () {
+  //   $('.filter-list').toggle();
+  // });
 
   // $('input, select').styler();
 
