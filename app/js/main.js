40ecac5f90dd45dc0f34ae84dd1cde8ac93d2b23
AlexeyLetov <adec78@mail.ru>
2020-10-01 21:32:54 +0700

    added homework 12

--- app/js/main.js
+++ app/js/main.js
@@ -9,14 +9,41 @@ $(function () {
     prevArrow: '<button class="slick-arrow slick-prev"><img src="images/icons/chevron-left.svg" alt="prev arrow"></button>',
     nextArrow: '<button class="slick-arrow slick-next"><img src="images/icons/chevron-right.svg" alt="next arrow"></button>',
     slidesToShow: 3,
-    slidesToScroll: 3
+    slidesToScroll: 3,
+    variableWidth: true,
+    responsive: [
+      {
+        breakpoint: 1200,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1
+        }
+      }
+      // {
+      //   breakpoint: 992,
+      //   settings: {
+      //     slidesToShow: 1,
+      //     slidesToScroll: 1
+      //   }
+      // }
+    ]
   });
 
   $(".feedback__inner").slick({
     prevArrow: '<button class="slick-arrow feedback__prev"><img src="images/icons/feedback-left.svg" alt="prev arrow"></button>',
     nextArrow: '<button class="slick-arrow feedback__next"><img src="images/icons/feedback-right.svg" alt="next arrow"></button>',
     slidesToShow: 2,
-    slidesToScroll: 2
+    slidesToScroll: 2,
+    variableWidth: true,
+    responsive: [
+      {
+        breakpoint: 1200,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
   });
 
   $(".rate-star").rateYo({
@@ -35,15 +62,12 @@ $(function () {
     readOnly: true
   });
 
-  // $('.title-box__filter').on("click", function () {
-  //   $('.filter-list').toggle();
-  // });
-
-  // $('input, select').styler();
+  $('.filter-text').on("click", function () {
+    $('.filter-list').toggle();
+  });
 
   if ($('.products__inner-box').length) {
     var mixer = mixitup('.products__inner-box');
   }
-
 
 });
